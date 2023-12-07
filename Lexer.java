d12b90e2eedd293bb8e0fd80c7be9fa71db055f6
MegaTophat <98920383+MegaTophat@users.noreply.github.com>
2023-12-07 02:17:25 -0600

    Reformat code and format to our grammar.

--- Lexer.java
+++ Lexer.java
@@ -1,301 +1,285 @@
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Lexer for the calc language
  */
 public class Lexer {
-  /// Source of the character stream
-  private InputStream file;
+    /// Source of the character stream
+    private final InputStream file;
 
-  /// Current character being matched
-  private char cur;
+    /// Current character being matched
+    private char cur;
 
-  /// Current Lexeme
-  private Lexeme curLex;
+    /// Current Lexeme
+    private Lexeme curLex;
 
-  /// Line and column number of input
-  private int line;
-  private int col;
-  private int startLine;
-  private int startCol;
-  private boolean eof;
+    /// Line and column number of input
+    private int line;
+    private int col;
+    private int startLine;
+    private int startCol;
+    private boolean eof;
 
-  /// The lexeme that we are accumulating
-  private StringBuilder curString;
+    /// The lexeme that we are accumulating
+    private StringBuilder curString;
 
-  /// Construct a lexer for the given input stream
-  public Lexer(InputStream file) {
-    this.file = file;
-    this.line = 1;
-    this.col = 0;
-    this.eof = false;
-    read();
-  }
-
-  private void setToken(Token tok) {
-    curLex = new Lexeme(tok, curString.toString(), startLine, startCol);
-  }
-
-  // skip characters we wish to ignore
-  private void skip() {
-    // skip all the blank characters
-    while (cur != '\n' && (Character.isSpaceChar(cur)|| cur == '#')) {
-      if(cur == '#') {
-        skipToEOL();
-      } else { 
+    /// Construct a lexer for the given input stream
+    public Lexer(InputStream file) {
+        this.file = file;
+        this.line = 1;
+        this.col = 0;
+        this.eof = false;
         read();
-      }
-    }
-  }
-
-  // skip to the end of the line
-  private void skipToEOL() {
-    while (!eof && cur != '\n') {
-      read();
-    }
-  }
-
-  public Lexeme next() {
-    // start the matching
-    skip();
-    curString = new StringBuilder();
-    startLine = line;
-    startCol = col;
-
-    // handle eof
-    if (eof) {
-      setToken(Token.EOF);
-      return curLex;
     }
 
-    if (match_single())
-      return curLex;
-    else if (match_number())
-      return curLex;
-    else if (matchWord())
-      return curLex;
-    else if(matchFixed()) 
-      return curLex;
-    else
-      consume();
+    public static void main(String[] args) throws FileNotFoundException {
+        final InputStream inputStream;
 
-    // invalid
-    setToken(Token.INVALID);
-    return curLex;
-  }
+        if (args.length > 0) {
+            final String workingDirectory = System.getProperty("user.dir");
+            File file = new File(workingDirectory, args[0]);
+            inputStream = new FileInputStream(file);
+        } else {
+            inputStream = System.in;
+        }
 
-  public Lexeme cur() {
-    return curLex;
-  }
+        Lexer lex = new Lexer(inputStream);
+        Lexeme tok;
 
-  /// Match our single character tokens
-  /// Return true on success, false on failure.
-  public boolean match_single() {
-    Token tok;
-
-    switch (cur) {
-      case '+':
-        tok = Token.PLUS;
-        break;
-      case '-':
-        tok = Token.MINUS;
-        break;
-      case '*':
-        tok = Token.TIMES;
-        break;
-      case '/':
-        tok = Token.DIVIDE;
-        break;
-      case '^':
-        tok = Token.POW;
-        break;
-      case '(':
-        tok = Token.LPAREN;
-        break;
-      case ')':
-        tok = Token.RPAREN;
-        break;
-      case '\n':
-        tok = Token.NEWLINE;
-        break;
-      case '=':
-        tok = Token.EQUAL;
-        break;
-      case '[':
-        tok = Token.LBRACKET;
-        break;
-      case ']':
-        tok = Token.RBRACKET;
-        break;
-      case '.':
-        tok = Token.DOT;
-        break;
-      default:
-        tok = Token.INVALID;
+        do {
+            tok = lex.next();
+            System.out.println(tok);
+        } while (tok.tok != TokenType.EOF);
     }
 
-    // did not match
-    if (tok == Token.INVALID) {
-      return false;
+    private void setToken(TokenType tok) {
+        curLex = new Lexeme(tok, curString.toString(), startLine, startCol);
     }
 
-    // match
-    consume();
-    setToken(tok);
-    return true;
-  }
-
-  /// Match an integer
-  private void consumeInteger() {
-    while (Character.isDigit(cur)) {
-      consume();
-    }
-  }
-
-  /// Match a Number
-  private boolean match_number() {
-    if (!Character.isDigit(cur))
-      return false;
-
-    // this is an integer
-    Token tok = Token.REALLIT;
-    consumeInteger();
-
-    // check for a dot
-    if (cur != '.') {
-      setToken(tok);
-      return true;
+    // skip characters we wish to ignore
+    private void skip() {
+        // skip all the blank characters
+        while (cur != '\n' && (Character.isSpaceChar(cur))) {
+            read();
+        }
     }
 
-    // it is a dot!
-    consume();
-    if (Character.isDigit(cur)) {
-      tok = Token.REALLIT;
-      consumeInteger();
-    } else {
-      tok = Token.INVALID;
-    }
-    setToken(tok);
-    return true;
-  }
-
-  private boolean matchWord() {
-    if (!Character.isAlphabetic(cur) && cur != '_')
-      return false;
-
-    // consume letters numbers and _
-    while (Character.isDigit(cur) || Character.isAlphabetic(cur) || cur == '_') {
-      consume();
+    // skip to the end of the line
+    private void skipToEOL() {
+        while (!eof && cur != '\n') {
+            read();
+        }
     }
 
-    // match against keywords
-    String str = curString.toString();
-    if (str.equals("MOD")) {
-      setToken(Token.MOD);
-    } else if (str.equals("display")) {
-      setToken(Token.DISPLAY);
-    } else if(str.equals("input")) {
-      setToken(Token.INPUT);
-    } else if(str.equals("sus")) {
-      setToken(Token.IF);
-    } else if(str.equals("end")) {
-      setToken(Token.END);
-    } else if(str.equals("while")) {
-      setToken(Token.WHILE);
-    } else if(str.equals("dimension")) {
-      setToken(Token.DIMENSION);
-    } else if(str.equals("record")) {
-      setToken(Token.RECORD);
-    } else if(str.equals("field")) {
-      setToken(Token.FIELD);
-    } else if(str.equals("fun")) {
-      setToken(Token.FUN);
-    } else {
-      //if it's not a keyword, it's an ID
-      setToken(Token.ID); 
+    Lexeme next() {
+        // start the matching
+        skip();
+        curString = new StringBuilder();
+        startLine = line;
+        startCol = col;
+
+        // handle eof
+        if (eof) {
+            setToken(TokenType.EOF);
+            return curLex;
+        }
+
+        if (match_single())
+            return curLex;
+        else if (match_number())
+            return curLex;
+        else if (matchWord())
+            return curLex;
+        else if (matchFixed())
+            return curLex;
+        else if (matchString()) {
+            return curLex;
+        } else
+            consume();
+
+        // invalid
+        setToken(TokenType.UNKNOWN);
+        return curLex;
     }
 
-    return true;
-  }
+    Lexeme cur() {
+        return curLex;
+    }
 
+    /// Match our single character tokens
+    /// Return true on success, false on failure.
+    public boolean match_single() {
+        final TokenType tok = switch (cur) {
+            case '+' -> TokenType.PLUS;
+            case '-' -> TokenType.MINUS;
+            case '*' -> TokenType.TIMES;
+            case '/' -> TokenType.DIVIDE;
+            case '^' -> TokenType.POW;
+            case '(' -> TokenType.LPAREN;
+            case ')' -> TokenType.RPAREN;
+            case '\n' -> TokenType.NEWLINE;
+            case '=' -> TokenType.EQUAL;
+            case '.' -> TokenType.DOT;
+            default -> TokenType.UNKNOWN;
+        };
 
-  /// Match a multi-character but fixed width
-  /// token
-  private boolean matchFixed() {
-    if(cur == '<') {
-      consume();
-      setToken(Token.LT);
+        // did not match
+        if (tok == TokenType.UNKNOWN) {
+            return false;
+        }
 
-      // check for the second part
-      if(cur == '=') {
+        // match
         consume();
-        setToken(Token.LTE);
-      } else if(cur == '>') {
+        setToken(tok);
+        return true;
+    }
+
+    /// Match an integer
+    private void consumeInteger() {
+        while (Character.isDigit(cur)) {
+            consume();
+        }
+    }
+
+    private void consumeString() {
+        while (this.cur != '"' && this.cur != '\n' && !this.eof) {
+            consume();
+        }
+    }
+
+    private boolean matchString() {
+        if (this.cur != '"') {
+            return false;
+        }
         consume();
-        setToken(Token.NE);
-      }
-      return true;
-    } else if(cur == '>') {
-      consume();
-      setToken(Token.GT);
-      if(cur == '=') {
+        consumeString();
+
+        if (this.cur != '"') {
+            setToken(TokenType.UNKNOWN);
+            return false;
+        }
+
+        // consume the ending " symbol
         consume();
-        setToken(Token.GTE);
-      }
-      return true;
+        setToken(TokenType.STRING);
+        return true;
     }
 
-    return false;
-  }
+    /// Match a Number
+    private boolean match_number() {
+        if (!Character.isDigit(cur))
+            return false;
 
-  /// Read the next character
-  private void read() {
-    // handle newline
-    if (cur == '\n') {
-      line++;
-      col = 0;
+        // this is an integer
+        consumeInteger();
+
+        // check for a dot
+        if (cur != '.') {
+            setToken(TokenType.NUMBER);
+            return true;
+        }
+
+        // consume the singular dot
+        consume();
+        if (Character.isDigit(cur)) {
+            consumeInteger();
+            setToken(TokenType.NUMBER);
+        } else {
+            setToken(TokenType.UNKNOWN);
+        }
+
+        return true;
     }
-    try {
-      int input = file.read();
-      cur = (char) input;
-      col++;
-      if (input == -1) {
-        eof = true;
-      }
-    } catch (IOException ex) {
-      // do nothing for now
-      eof = true;
+
+    private boolean matchWord() {
+        if (!Character.isAlphabetic(cur) && cur != '_')
+            return false;
+
+        // consume letters numbers and _
+        while (Character.isDigit(cur) || Character.isAlphabetic(cur) || cur == '_') {
+            consume();
+        }
+
+        // match keywords, then variable names as lowest priority
+        final String str = curString.toString();
+        switch (str) {
+            case "MOD" -> setToken(TokenType.MOD);
+            case "PRINT" -> setToken(TokenType.PRINT);
+            case "READ" -> setToken(TokenType.READ);
+            case "sus" -> setToken(TokenType.SUS);
+            case "swotus" -> setToken(TokenType.SWOTUS);
+            case "susmoney" -> setToken(TokenType.SUSMONEY);
+            case "ARRAY" -> setToken(TokenType.ARRAY);
+            default ->
+                //if it's not a keyword, it's a valid variable name
+                    setToken(TokenType.VARIABLE_NAME);
+        }
+
+        return true;
     }
-  }
 
-  /// Insert the current character into the curStr
-  /// and advanced the lexer
-  private void consume() {
-    curString.append(cur);
-    read();
-  }
+    /// Match a multi-character but fixed width
+    /// token
+    private boolean matchFixed() {
+        if (cur == '<') {
+            consume();
+            setToken(TokenType.LESS_THAN);
 
-  public static void main(String[] args) throws FileNotFoundException {
-    final InputStream inputStream;
-    
-    if (args.length > 0) {
-      final String workingDirectory = System.getProperty("user.dir");
-      File file = new File(workingDirectory, args[0]);
-      inputStream = new FileInputStream(file);
-    } else {
-      inputStream = System.in;
+            // check for the second part
+            if (cur == '=') {
+                consume();
+                setToken(TokenType.LESS_THAN_OR_EQUAL);
+            }
+
+            return true;
+        } else if (cur == '>') {
+            consume();
+            setToken(TokenType.GREATER_THAN);
+            if (cur == '=') {
+                consume();
+                setToken(TokenType.GREATER_THAN_OR_EQUAL);
+            }
+
+            return true;
+        } else if (cur == '!') {
+            consume();
+            if (cur == '=') {
+                consume();
+                setToken(TokenType.NOT_EQUAL);
+            }
+        }
+
+        return false;
     }
-    
-    Lexer lex = new Lexer(inputStream);
-    Lexeme tok;
 
-    do {
-      tok = lex.next();
-      System.out.println(tok);
-    } while (tok.tok != Token.EOF);
-  }
+    /// Read the next character
+    private void read() {
+        // handle newline
+        if (cur == '\n') {
+            line++;
+            col = 0;
+        }
+        try {
+            int input = file.read();
+            cur = (char) input;
+            col++;
+            if (input == -1) {
+                eof = true;
+            }
+        } catch (IOException ex) {
+            // do nothing for now
+            eof = true;
+        }
+    }
+
+    /// Insert the current character into the curStr
+    /// and advanced the lexer
+    private void consume() {
+        curString.append(cur);
+        read();
+    }
 }
